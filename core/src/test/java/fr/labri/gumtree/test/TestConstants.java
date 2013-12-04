cfe258fbd0dde015b6f9f4aa95691cc9d3496974
Jean-Rémy Falleri <jr.falleri@laposte.net>
2013-12-04 10:25:57 +0100

    Fixed tests.

--- core/src/test/java/fr/labri/gumtree/test/TestConstants.java
+++ core/src/test/java/fr/labri/gumtree/test/TestConstants.java
@@ -2,9 +2,9 @@ package fr.labri.gumtree.test;
 
 public class TestConstants {
 	
-	public static final String DUMMY_SRC = "../data/samples/raw/Dummy_src.xml";
+	public static final String DUMMY_SRC = "../samples/raw/Dummy_v0.xml";
 	
-	public static final String DUMMY_DST = "../data/samples/raw/Dummy_dst.xml";
+	public static final String DUMMY_DST = "../samples/raw/Dummy_v1.xml";
 
 
 }
