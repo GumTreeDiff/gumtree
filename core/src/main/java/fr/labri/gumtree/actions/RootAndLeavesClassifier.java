b8f7bc14ffb6e794db8f9ca52d76dae592668c33
Jean-Rémy Falleri <jr.falleri@laposte.net>
2013-12-14 11:42:34 +0100

    Better rendering of modifications.

--- core/src/main/java/fr/labri/gumtree/actions/RootAndLeavesClassifier.java
+++ core/src/main/java/fr/labri/gumtree/actions/RootAndLeavesClassifier.java
@@ -15,10 +15,6 @@ import fr.labri.gumtree.tree.Tree;
 
 public class RootAndLeavesClassifier extends TreeClassifier {
 	
-	private Set<Tree> srcModifiedTrees;
-
-	private Set<Tree> dstModifiedTrees;
-	
 	public RootAndLeavesClassifier(Tree src, Tree dst, Set<Mapping> rawMappings, List<Action> actions) {
 		super(src, dst, rawMappings, actions);
 	}
@@ -27,55 +23,34 @@ public class RootAndLeavesClassifier extends TreeClassifier {
 		super(src, dst, m);
 	}
 
-
 	@Override
 	public void classify() {
-		srcModifiedTrees = new HashSet<>();
-		dstModifiedTrees = new HashSet<>();
 		for (Action a: actions) {
 			if (a instanceof Insert) {
-				dstModifiedTrees.add(a.getNode());
 				dstAddTrees.add(a.getNode());
 			} else if (a instanceof Delete) {
-				srcModifiedTrees.add(a.getNode());
 				srcDelTrees.add(a.getNode());
 			} else if (a instanceof Update) {
-				srcModifiedTrees.add(a.getNode());
 				srcUpdTrees.add(a.getNode());
-				dstModifiedTrees.add(mappings.getDst(a.getNode()));
 				dstUpdTrees.add(mappings.getDst(a.getNode()));
 			} else if (a instanceof Move) {
-				srcModifiedTrees.add(a.getNode());
 				srcMvTrees.add(a.getNode());
-				dstModifiedTrees.add(mappings.getDst(a.getNode()));
 				dstMvTrees.add(mappings.getDst(a.getNode()));
 			}	
 		}
-		
-		Set<Tree> fDstAddTrees = new HashSet<>(dstAddTrees);
-		for (Tree t: dstAddTrees)
-			if (dstAddTrees.containsAll(t.getDescendants()))
-				fDstAddTrees.removeAll(t.getDescendants());
+
+		Set<Tree> fDstAddTrees = new HashSet<>();
+		for (Tree t: dstAddTrees) 
+			if (!dstAddTrees.contains(t.getParent()))
+				fDstAddTrees.add(t);
 		dstAddTrees = fDstAddTrees;
 		
-		Set<Tree> fSrcDelTrees = new HashSet<>(srcDelTrees);
+		Set<Tree> fSrcDelTrees = new HashSet<>();
 		for (Tree t: srcDelTrees) {
-			if (srcDelTrees.containsAll(t.getDescendants()))
-				fSrcDelTrees.removeAll(t.getDescendants());
+			if (!srcDelTrees.contains(t.getParent()))
+				fSrcDelTrees.add(t);
 		}
 		srcDelTrees = fSrcDelTrees;
 	}
 	
-	public int dstDepth(Tree dst) {
-		int d = 1;
-		for (Tree p: dst.getParents()) if (dstModifiedTrees.contains(p)) d++;
-		return d;
-	}
-	
-	public int srcDepth(Tree src) {
-		int d = 1;
-		for (Tree p: src.getParents()) if (srcModifiedTrees.contains(p)) d++;
-		return d;
-	}
-
 }
