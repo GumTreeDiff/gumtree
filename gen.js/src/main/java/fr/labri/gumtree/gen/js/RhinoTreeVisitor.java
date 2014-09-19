0b67ab54c823ccd40df4e5e5609bb0034a4fc5e4
Floreal Morandat florealm@gmail.com <florealm@gmail.com>
2014-09-19 12:31:01 +0200

    fixed RhinoParser for new typeLabel system

--- gen.js/src/main/java/fr/labri/gumtree/gen/js/RhinoTreeVisitor.java
+++ gen.js/src/main/java/fr/labri/gumtree/gen/js/RhinoTreeVisitor.java
@@ -52,7 +52,7 @@ public class RhinoTreeVisitor implements NodeVisitor {
 	}
 	
 	private Tree buildTree(AstNode node)  {
-		Tree t = new Tree(node.getType(), Token.typeToName(node.getType()));
+		Tree t = new Tree(node.getType(), Tree.NO_LABEL, Token.typeToName(node.getType()));
 		t.setPos(node.getAbsolutePosition());
 		t.setLength(node.getLength());
 		trees.put(node, t);
