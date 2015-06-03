c9457cb8f030767dca594ac50f1d03145bebab4b
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-06-03 15:20:45 -0700

    Enabled Java 8 compliance
    And I added a test to make sure it works fine.

---
+++ gen.jdt/src/test/java/gen/jdt/Java8Test.java
@@ -0,0 +1,26 @@
+package gen.jdt;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
+import org.junit.Test;
+
+import fr.labri.gumtree.gen.jdt.JdtTreeGenerator;
+import fr.labri.gumtree.tree.Tree;
+
+public class Java8Test {
+	
+	@Test
+	public void testJava8Syntax() throws IOException {
+		Path path = Files.createTempFile("", ".java");
+		String input = "public class A{public void m(){new ArrayList<Object>().stream().forEach(a -> {});}}";
+		Files.write(path, input.getBytes(), StandardOpenOption.WRITE);
+		Tree tree = new JdtTreeGenerator().fromFile(path.toAbsolutePath().toString());
+		assertEquals(24, tree.getSize());
+		path.toFile().delete();
+	}
+}
