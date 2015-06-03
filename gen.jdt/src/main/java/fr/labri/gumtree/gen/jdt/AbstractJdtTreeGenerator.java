89ad4d094473c3c70fb6bcec9fdbe00aaf14efb0
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-06-03 15:20:45 -0700

    Enabled Java 8 compliance
    And I added a test to make sure it works fine.

--- gen.jdt/src/main/java/fr/labri/gumtree/gen/jdt/AbstractJdtTreeGenerator.java
+++ gen.jdt/src/main/java/fr/labri/gumtree/gen/jdt/AbstractJdtTreeGenerator.java
@@ -13,12 +13,12 @@ public abstract class AbstractJdtTreeGenerator extends TreeGenerator {
 	
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public Tree generate(String file) {
-		ASTParser parser = ASTParser.newParser(AST.JLS3);
+		ASTParser parser = ASTParser.newParser(AST.JLS8);
 		parser.setKind(ASTParser.K_COMPILATION_UNIT);
 		Map pOptions = JavaCore.getOptions();
-		pOptions.put(JavaCore.COMPILER_COMPLIANCE, JavaCore.VERSION_1_6);
-		pOptions.put(JavaCore.COMPILER_CODEGEN_TARGET_PLATFORM, JavaCore.VERSION_1_6);
-		pOptions.put(JavaCore.COMPILER_SOURCE, JavaCore.VERSION_1_6);
+		pOptions.put(JavaCore.COMPILER_COMPLIANCE, JavaCore.VERSION_1_8);
+		pOptions.put(JavaCore.COMPILER_CODEGEN_TARGET_PLATFORM, JavaCore.VERSION_1_8);
+		pOptions.put(JavaCore.COMPILER_SOURCE, JavaCore.VERSION_1_8);
 		parser.setCompilerOptions(pOptions);
 		Requestor req = new Requestor(createVisitor());
 		parser.createASTs(new String[] { file }, null, new String[] {}, req, null);
