eb806f0427a06f8e1695b70cffd179b08fda003c
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-06-04 00:44:42 -0700

    Extracted method to configure the parser.
    # Conflicts:
    #	gen.jdt/src/main/java/fr/labri/gumtree/gen/jdt/AbstractJdtTreeGenerator.java

--- gen.jdt/src/main/java/fr/labri/gumtree/gen/jdt/AbstractJdtTreeGenerator.java
+++ gen.jdt/src/main/java/fr/labri/gumtree/gen/jdt/AbstractJdtTreeGenerator.java
@@ -11,18 +11,22 @@ import fr.labri.gumtree.tree.Tree;
 
 public abstract class AbstractJdtTreeGenerator extends TreeGenerator {
 	
-	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public Tree generate(String file) {
-		ASTParser parser = ASTParser.newParser(AST.JLS3);
+		ASTParser parser = ASTParser.newParser(AST.JLS8);
+		configureParser(parser);
+		Requestor req = new Requestor(createVisitor());
+		parser.createASTs(new String[] { file }, null, new String[] {}, req, null);
+		return req.getVisitor().getTree();
+	}
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	private void configureParser(ASTParser parser) {
 		parser.setKind(ASTParser.K_COMPILATION_UNIT);
 		Map pOptions = JavaCore.getOptions();
 		pOptions.put(JavaCore.COMPILER_COMPLIANCE, JavaCore.VERSION_1_6);
 		pOptions.put(JavaCore.COMPILER_CODEGEN_TARGET_PLATFORM, JavaCore.VERSION_1_6);
 		pOptions.put(JavaCore.COMPILER_SOURCE, JavaCore.VERSION_1_6);
 		parser.setCompilerOptions(pOptions);
-		Requestor req = new Requestor(createVisitor());
-		parser.createASTs(new String[] { file }, null, new String[] {}, req, null);
-		return req.getVisitor().getTree();
 	}
 
 	@Override
