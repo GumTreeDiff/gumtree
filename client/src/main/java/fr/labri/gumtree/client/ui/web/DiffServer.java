af2eb58f3efef3d9d7c7caf0ee7fa1ec71e70386
Jean-Rémy Falleri <jr.falleri@gmail.com>
2015-06-06 13:32:01 +0200

    now uses NanoHTTPD from Maven. also updated code to use ServerRunner.

--- client/src/main/java/fr/labri/gumtree/client/ui/web/DiffServer.java
+++ client/src/main/java/fr/labri/gumtree/client/ui/web/DiffServer.java
@@ -6,10 +6,10 @@ import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
+import fi.iki.elonen.NanoHTTPD;
 import org.rendersnake.HtmlCanvas;
 import org.rendersnake.Renderable;
 
-import fr.labri.gumtree.client.ui.web.NanoHTTPD.Response.Status;
 import fr.labri.gumtree.client.ui.web.views.DirectoryComparatorView;
 import fr.labri.gumtree.client.ui.web.views.DiffView;
 import fr.labri.gumtree.client.ui.web.views.ScriptView;
@@ -17,63 +17,40 @@ import fr.labri.gumtree.io.DirectoryComparator;
 import fr.labri.gumtree.tree.Pair;
 
 public class DiffServer extends NanoHTTPD {
-
-	public static final int PORT = 4754;
 	
 	public DirectoryComparator comparator;
 	
-	public DiffServer(String src, String dst) {
-		super(PORT);
+	public DiffServer(String src, String dst, int port) {
+		super(port);
 		comparator = new DirectoryComparator(src, dst);
 		comparator.compare();
 	}
-	
-    public static void start(NanoHTTPD server) {
-        try {
-            server.start();
-        } catch (IOException e) {
-            System.err.println("Couldn't start server:" + e);
-            System.exit(-1);
-        }
-
-        System.out.println("Server running on url: http://localhost:4754");
-        try {
-        	System.in.read();
-        } catch (Throwable ignored) {
-        	ignored.printStackTrace();
-        }
-
-        server.stop();
-        System.out.println("Server stopped.\n");
-    }
 
 	@Override
-	public Response serve(String uri, Method method, Map<String, String> header, Map<String, String> parms, Map<String, String> files) {
+	public Response serve(IHTTPSession session) {
+		String uri = session.getUri();
+		Map<String, String> parms = session.getParms();
 		try {
-			if ("/list".equals(uri)) {
+			if ("/list".equals(uri) || ("/".equals(uri) && comparator.isDirMode())) {
 				DirectoryComparatorView view = new DirectoryComparatorView(comparator);
 				return respond(view);
-			} else if ("/diff".equals(uri)) {
-				int id = Integer.parseInt(parms.get("id"));
+			} else if ("/diff".equals(uri) || ("/".equals(uri) && !comparator.isDirMode())) {
+				int id = 0;
+				if (parms.containsKey("id"))
+					id = Integer.parseInt(parms.get("id"));
 				Pair<File, File> pair = comparator.getModifiedFiles().get(id);
 				return respond(new DiffView(pair.getFirst(), pair.getSecond()));
 			} else if ("/script".equals(uri)) {
 				int id = Integer.parseInt(parms.get("id"));
 				Pair<File, File> pair = comparator.getModifiedFiles().get(id);
 				return respond(new ScriptView(pair.getFirst(), pair.getSecond()));
-			} else if ("/quit".equals(uri)) System.exit(0);
+			} else if ("/quit".equals(uri))
+				System.exit(0);
 			else if (uri.startsWith("/res/")) {
 				String res = uri.substring(1);
 				InputStream data = ClassLoader.getSystemClassLoader().getResourceAsStream(res);
-				if (uri.endsWith(".css")) return new Response(Status.OK, "text/css", data);
-				else if (uri.endsWith(".js")) return new Response(Status.OK, "text/javascript", data);
-			} else if ("/".equals(uri)) {
-				if (comparator.isDirMode()) return serve("/list", method, header, parms, files);
-				else {
-					parms = new HashMap<String, String>();
-					parms.put("id", "0");
-					return serve("/diff", method, header, parms, files);
-				}
+				if (uri.endsWith(".css")) return new Response(Response.Status.OK, "text/css", data);
+				else if (uri.endsWith(".js")) return new Response(Response.Status.OK, "text/javascript", data);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -97,11 +74,11 @@ public class DiffServer extends NanoHTTPD {
 	}
 	
 	private Response respond(String mimeType, String s) {
-		return new Response(Status.OK, mimeType, s);
+		return new Response(Response.Status.OK, mimeType, s);
 	}
 
 	private Response respond(String mimeType, InputStream s) {
-		return new Response(Status.OK, mimeType, s);
+		return new Response(Response.Status.OK, mimeType, s);
 	}
 
 }
--- client/src/main/java/fr/labri/gumtree/client/ui/web/WebDiff.java
+++ client/src/main/java/fr/labri/gumtree/client/ui/web/WebDiff.java
@@ -1,18 +1,26 @@
 package fr.labri.gumtree.client.ui.web;
 
+import fi.iki.elonen.ServerRunner;
 import fr.labri.gumtree.client.DiffClient;
 import fr.labri.gumtree.client.DiffOptions;
 
+import java.io.IOException;
+
 public class WebDiff extends DiffClient {
+
+	public static final int DEFAULT_PORT = 4754;
+
+	private DiffServer server;
 
 	public WebDiff(DiffOptions diffOptions) {
 		super(diffOptions);
+		server = new DiffServer(diffOptions.getSrc(), diffOptions.getDst(), DEFAULT_PORT);
 	}
 
 	@Override
 	public void start() {
-		DiffServer server = new DiffServer(diffOptions.getSrc(), diffOptions.getDst());
-		DiffServer.start(server);
+		System.out.println(String.format("Starting server: %s", "http://127.0.0.1:" + DEFAULT_PORT));
+		ServerRunner.executeInstance(server);
 	}
 
 }
