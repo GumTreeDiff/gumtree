4447dbbaa7a6d366b8b6593a486d9483f0f6feda
Jean-Rémy Falleri <jr.falleri@gmail.com>
2015-06-20 01:19:13 +0200

    checkstyle all ok!

--- gen.js/src/test/java/fr/labri/gumtree/gen/js/TestJsGenerator.java
+++ gen.js/src/test/java/fr/labri/gumtree/gen/js/TestJsGenerator.java
@@ -11,21 +11,21 @@ import org.junit.Test;
 import fr.labri.gumtree.tree.ITree;
 
 public class TestJsGenerator {
-	
-	@Test
-	public void testFileParsing() throws IOException {
-		Reader r = new InputStreamReader(getClass().getResourceAsStream("/sample.js"));
-		ITree tree = new RhinoTreeGenerator().generateFromReader(r).getRoot();
-		assertEquals(136, tree.getType());
-		assertEquals(401, tree.getSize());
-	}
-	
-	@Test
-	public void testSimpleSyntax() throws IOException {
-		String input = "console.log(\"Hello world!\");";
-		ITree tree = new RhinoTreeGenerator().generateFromString(input).getRoot();
-		assertEquals(136, tree.getType());
-		assertEquals(7, tree.getSize());
-	}
+
+    @Test
+    public void testFileParsing() throws IOException {
+        Reader r = new InputStreamReader(getClass().getResourceAsStream("/sample.js"));
+        ITree tree = new RhinoTreeGenerator().generateFromReader(r).getRoot();
+        assertEquals(136, tree.getType());
+        assertEquals(401, tree.getSize());
+    }
+
+    @Test
+    public void testSimpleSyntax() throws IOException {
+        String input = "console.log(\"Hello world!\");";
+        ITree tree = new RhinoTreeGenerator().generateFromString(input).getRoot();
+        assertEquals(136, tree.getType());
+        assertEquals(7, tree.getSize());
+    }
 
 }
