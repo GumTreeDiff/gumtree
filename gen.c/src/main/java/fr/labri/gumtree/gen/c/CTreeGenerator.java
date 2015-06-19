345449cba2ade91f1c53a879b7b865100ef33b44
Jean-Rémy Falleri <jr.falleri@gmail.com>
2015-06-19 15:45:28 +0200

    checkstyled gen.c.

--- gen.c/src/main/java/fr/labri/gumtree/gen/c/CTreeGenerator.java
+++ gen.c/src/main/java/fr/labri/gumtree/gen/c/CTreeGenerator.java
@@ -10,41 +10,41 @@ import java.io.*;
 @Register(id = "c-cocci", accept = "\\.[ch]$")
 public class CTreeGenerator extends TreeGenerator {
 
-	private static final String COCCI_CMD = "cgum";
+    private static final String COCCI_CMD = "cgum";
 
-	@Override
-	public TreeContext generate(Reader r) throws IOException {
-		//FIXME this is not efficient but I am not sure how to speed up things here.
-		File f = File.createTempFile("gumtree", ".c");
-		System.out.println(f.getAbsolutePath());
-		FileWriter w = new FileWriter(f);
-		BufferedReader br = new BufferedReader(r);
-		String line = br.readLine();
+    @Override
+    public TreeContext generate(Reader r) throws IOException {
+        //FIXME this is not efficient but I am not sure how to speed up things here.
+        File f = File.createTempFile("gumtree", ".c");
+        System.out.println(f.getAbsolutePath());
+        FileWriter w = new FileWriter(f);
+        BufferedReader br = new BufferedReader(r);
+        String line = br.readLine();
         while (line != null) {
             w.append(line);
             w.append(System.lineSeparator());
             line = br.readLine();
         }
-		w.close();
-		br.close();
-		ProcessBuilder b = new ProcessBuilder(COCCI_CMD, f.getAbsolutePath());
-		b.directory(f.getParentFile());
-		try {
-			Process p = b.start();
-			StringBuffer buf = new StringBuffer();
-			br = new BufferedReader(new InputStreamReader(p.getInputStream()));
-			line = null;
-			while ((line = br.readLine()) != null)
-				buf.append(line + "\n");
-			p.waitFor();
-			if (p.exitValue() != 0)  throw new RuntimeException();
-			r.close();
-			String xml = buf.toString();
-			return TreeIoUtils.fromXmlString(xml);
-		} catch (InterruptedException e) {
-			throw new RuntimeException(e);
-		} finally {
-			f.delete();
-		}
-	}
+        w.close();
+        br.close();
+        ProcessBuilder b = new ProcessBuilder(COCCI_CMD, f.getAbsolutePath());
+        b.directory(f.getParentFile());
+        try {
+            Process p = b.start();
+            StringBuffer buf = new StringBuffer();
+            br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            line = null;
+            while ((line = br.readLine()) != null)
+                buf.append(line + "\n");
+            p.waitFor();
+            if (p.exitValue() != 0)  throw new RuntimeException();
+            r.close();
+            String xml = buf.toString();
+            return TreeIoUtils.fromXmlString(xml);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            f.delete();
+        }
+    }
 }
--- gen.c/src/test/java/fr/labri/gumtree/gen/c/TestCGenerator.java
+++ gen.c/src/test/java/fr/labri/gumtree/gen/c/TestCGenerator.java
@@ -8,14 +8,13 @@ import static org.junit.Assert.*;
 import fr.labri.gumtree.tree.ITree;
 
 public class TestCGenerator {
-	
-	@Test
-	public void testSimpleSyntax() throws IOException {
-		String input = "int main() { printf(\"Hello world!\"); return 0; }";
-		// @TODO find a way to not depend on cgum binary.
-		// ITree t = new CTreeGenerator().generateFromString(input).getRoot();
-		// assertEquals(450000, t.getType());
 
-	}
-	
+    @Test
+    public void testSimpleSyntax() throws IOException {
+        String input = "int main() { printf(\"Hello world!\"); return 0; }";
+        // @TODO find a way to not depend on cgum binary.
+        // ITree t = new CTreeGenerator().generateFromString(input).getRoot();
+        // assertEquals(450000, t.getType());
+    }
+
 }
