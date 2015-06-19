c463987adb239a0a797ca51034aadebe43693ff6
Jean-Rémy Falleri <jr.falleri@gmail.com>
2015-06-19 15:22:35 +0200

    checkstyle on gen.sax.

--- gen.sax/src/test/java/fr/labri/gumtree/gen/sax/TestParsing.java
+++ gen.sax/src/test/java/fr/labri/gumtree/gen/sax/TestParsing.java
@@ -7,11 +7,10 @@ import org.junit.Test;
 import java.io.InputStreamReader;
 
 public class TestParsing {
-	@Test
-	public void testA1() throws Exception {
-		TreeContext tc = new SAXTreeGenerator().generateFromReader(new InputStreamReader(getClass().getResourceAsStream("/action_v0.xml")));
-		TreeIoUtils.toXml(tc).writeTo(System.out);
-//		TreeIoUtils.lispSerializer().toStream(tc, System.out);
-//		TreeIoUtils.lispSerializer(tc).toString(tc);
-	}
+    @Test
+    public void testA1() throws Exception {
+        TreeContext tc = new SaxTreeGenerator().generateFromReader(
+                new InputStreamReader(getClass().getResourceAsStream("/action_v0.xml")));
+        TreeIoUtils.toXml(tc).writeTo(System.out);
+    }
 }
