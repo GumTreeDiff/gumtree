13e6e200d68ec87d48bd6cc27d1a4de637f4be48
Jean-Rémy Falleri <jr.falleri@gmail.com>
2016-01-30 10:31:47 +0100

    added textual action diff.

---
+++ client.diff/src/main/java/com/github/gumtreediff/client/diff/AdhocDiff.java
@@ -0,0 +1,57 @@
+/*
+ * This file is part of GumTree.
+ *
+ * GumTree is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GumTree is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with GumTree.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2011-2015 Jean-Rémy Falleri <jr.falleri@gmail.com>
+ * Copyright 2011-2015 Floréal Morandat <florealm@gmail.com>
+ */
+
+package com.github.gumtreediff.client.diff;
+
+import com.github.gumtreediff.actions.ActionGenerator;
+import com.github.gumtreediff.actions.model.Action;
+import com.github.gumtreediff.client.Register;
+import com.github.gumtreediff.io.ActionsIoUtils;
+import com.github.gumtreediff.matchers.Matcher;
+
+import java.util.List;
+
+@Register(name = "adhocdiff", description = "Dump actions in our textual format",
+        options = AbstractDiffClient.Options.class)
+public class AdhocDiff extends AbstractDiffClient<AbstractDiffClient.Options> {
+
+    public AdhocDiff(String[] args) {
+        super(args);
+    }
+
+    @Override
+    protected Options newOptions() {
+        return new Options();
+    }
+
+    @Override
+    public void run() {
+        Matcher m = matchTrees();
+        ActionGenerator g = new ActionGenerator(getSrcTreeContext().getRoot(),
+            getDstTreeContext().getRoot(), m.getMappings());
+        g.generate();
+        List<Action> actions = g.getActions();
+        try {
+            ActionsIoUtils.toText(getSrcTreeContext(), actions, m.getMappings()).writeTo(System.out);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
