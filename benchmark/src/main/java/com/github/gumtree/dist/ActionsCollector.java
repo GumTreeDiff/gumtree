f33a706d6764bd440026a20192f98f1a5605062b
Jean-Rémy Falleri <jr.falleri@gmail.com>
2016-07-21 10:51:42 +0200

    updated gradle build and added missing copyright.

--- benchmark/src/main/java/com/github/gumtree/dist/ActionsCollector.java
+++ benchmark/src/main/java/com/github/gumtree/dist/ActionsCollector.java
@@ -1,3 +1,22 @@
+/*
+ * This file is part of GumTree.
+ *
+ * GumTree is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GumTree is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with GumTree.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2016 Jean-Rémy Falleri <jr.falleri@gmail.com>
+ */
+
 package com.github.gumtree.dist;
 
 import com.github.gumtreediff.actions.ActionGenerator;
@@ -20,11 +39,11 @@ import java.util.stream.Collectors;
 
 public class ActionsCollector {
 
-    private final static String OUTPUT_DIR = "build/tmp/actions/";
+    private static final String OUTPUT_DIR = "build/tmp/actions/";
 
-    private final static String RES_DIR = "src/jmh/resources/";
+    private static final String RES_DIR = "src/jmh/resources/";
 
-    private final static String REF_DIR = "src/main/resources/";
+    private static final String REF_DIR = "src/main/resources/";
 
     public static void main(String[] args) throws Exception {
         if (args.length != 1) {
@@ -43,14 +62,16 @@ public class ActionsCollector {
 
     public static void collectActions() throws Exception {
         Run.initGenerators();
-        List<Path> paths = Files.walk(Paths.get(RES_DIR)).filter(p -> p.getFileName().toString().matches(".*_v0_.*\\.xml")).collect(Collectors.toList());
+        List<Path> paths = Files.walk(Paths.get(RES_DIR)).filter(
+                p -> p.getFileName().toString().matches(".*_v0_.*\\.xml")).collect(Collectors.toList());
         Files.createDirectories(Paths.get(OUTPUT_DIR));
         for (Path path : paths) {
             Path otherPath = Paths.get(path.toString().replace("_v0_","_v1_"));
             Path outputPath = Paths.get(path.toString().replace("_v0_","_actions_"));
             TreeContext src = TreeIoUtils.fromXml().generateFromFile(path.toString());
             TreeContext dst = TreeIoUtils.fromXml().generateFromFile(otherPath.toString());
-            CompositeMatchers.ClassicGumtree matcher = new CompositeMatchers.ClassicGumtree(src.getRoot(), dst.getRoot(), new MappingStore());
+            CompositeMatchers.ClassicGumtree matcher = new CompositeMatchers.ClassicGumtree(
+                    src.getRoot(), dst.getRoot(), new MappingStore());
             matcher.match();
             ActionGenerator g = new ActionGenerator(src.getRoot(), dst.getRoot(), matcher.getMappings());
             List<Action> actions = g.generate();
@@ -62,12 +83,14 @@ public class ActionsCollector {
 
     public static void checkActions() throws Exception {
         Run.initGenerators();
-        List<Path> paths = Files.walk(Paths.get(RES_DIR)).filter(p -> p.getFileName().toString().matches(".*_v0_.*\\.xml")).collect(Collectors.toList());
+        List<Path> paths = Files.walk(Paths.get(RES_DIR)).filter(
+                p -> p.getFileName().toString().matches(".*_v0_.*\\.xml")).collect(Collectors.toList());
         for (Path path : paths) {
             Path otherPath = Paths.get(path.toString().replace("_v0_","_v1_"));
             TreeContext src = TreeIoUtils.fromXml().generateFromFile(path.toString());
             TreeContext dst = TreeIoUtils.fromXml().generateFromFile(otherPath.toString());
-            CompositeMatchers.ClassicGumtree matcher = new CompositeMatchers.ClassicGumtree(src.getRoot(), dst.getRoot(), new MappingStore());
+            CompositeMatchers.ClassicGumtree matcher = new CompositeMatchers.ClassicGumtree(
+                    src.getRoot(), dst.getRoot(), new MappingStore());
             matcher.match();
             ActionGenerator g = new ActionGenerator(src.getRoot(), dst.getRoot(), matcher.getMappings());
             List<Action> actions = g.generate();
--- benchmark/src/main/java/com/github/gumtree/dist/BenchmarkCollector.java
+++ benchmark/src/main/java/com/github/gumtree/dist/BenchmarkCollector.java
@@ -1,3 +1,22 @@
+/*
+ * This file is part of GumTree.
+ *
+ * GumTree is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GumTree is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with GumTree.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2016 Jean-Rémy Falleri <jr.falleri@gmail.com>
+ */
+
 package com.github.gumtree.dist;
 
 import com.github.gumtreediff.client.Run;
@@ -15,7 +34,7 @@ import java.util.stream.Collectors;
 
 public class BenchmarkCollector {
 
-    private final static String OUTPUT_DIR = "build/tmp/trees/";
+    private static final String OUTPUT_DIR = "build/tmp/trees/";
 
     public static void main(String[] args) throws Exception {
         if (args.length != 1) {
@@ -27,7 +46,8 @@ public class BenchmarkCollector {
 
     public static void collectTrees(String dir) throws Exception {
         Run.initGenerators();
-        List<Path> paths = Files.walk(Paths.get(dir)).filter(p -> p.getFileName().toString().matches(".*_v0\\.(java|js|rb|c)")).collect(Collectors.toList());
+        List<Path> paths = Files.walk(Paths.get(dir)).filter(
+                p -> p.getFileName().toString().matches(".*_v0\\.(java|js|rb|c)")).collect(Collectors.toList());
         Files.createDirectories(Paths.get(OUTPUT_DIR));
         for (Path path : paths) {
             Path otherPath = Paths.get(path.toString().replace("_v0.","_v1."));
