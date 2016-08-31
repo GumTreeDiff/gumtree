c115ce39036084613f450b87638594c39578f6ed
Jean-Rémy Falleri <jr.falleri@gmail.com>
2016-08-31 13:11:30 +0200

    now gen.c is testable with travis.

--- gen.c/src/test/java/com/github/gumtreediff/gen/c/TestCGenerator.java
+++ gen.c/src/test/java/com/github/gumtreediff/gen/c/TestCGenerator.java
@@ -22,6 +22,7 @@ package com.github.gumtreediff.gen.c;
 
 import java.io.IOException;
 
+import org.junit.Assert;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -32,9 +33,8 @@ public class TestCGenerator {
     @Test
     public void testSimpleSyntax() throws IOException {
         String input = "int main() { printf(\"Hello world!\"); return 0; }";
-        // @TODO find a way to not depend on cgum binary.
-        // ITree t = new CTreeGenerator().generateFromString(input).getRoot();
-        // assertEquals(450000, t.getType());
+        ITree t = new CTreeGenerator().generateFromString(input).getRoot();
+        Assert.assertEquals(18, t.getSize());
     }
 
 }
