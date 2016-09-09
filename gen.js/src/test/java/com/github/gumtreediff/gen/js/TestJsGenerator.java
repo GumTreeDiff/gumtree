bee7903c258ce88fd07f7915244be4c2a2250985
Jean-Rémy Falleri <jr.falleri@gmail.com>
2016-09-09 14:41:49 +0200

    fixed JS tests.

--- gen.js/src/test/java/com/github/gumtreediff/gen/js/TestJsGenerator.java
+++ gen.js/src/test/java/com/github/gumtreediff/gen/js/TestJsGenerator.java
@@ -33,26 +33,24 @@ import com.github.gumtreediff.tree.ITree;
 public class TestJsGenerator {
 
     @Test
-    public void testFileParsing() throws IOException {
-        Reader r = new InputStreamReader(getClass().getResourceAsStream("/sample.js"));
-        ITree tree = new RhinoTreeGenerator().generateFromReader(r).getRoot();
-        assertEquals(136, tree.getType());
-        assertEquals(401, tree.getSize());
-    }
-
-    @Test
-    public void testSimpleSyntax() throws IOException {
+    public void testStatement() throws IOException {
         String input = "console.log(\"Hello world!\");";
         ITree tree = new RhinoTreeGenerator().generateFromString(input).getRoot();
-        assertEquals(136, tree.getType());
         assertEquals(7, tree.getSize());
     }
 
     @Test
-    public void testComments() throws IOException {
+    public void testComment() throws IOException {
         String input = "console.log(\"Hello world!\"); /* with comment */";
         ITree tree = new RhinoTreeGenerator().generateFromString(input).getRoot();
         assertEquals(8, tree.getSize());
+    }
+
+    @Test
+    public void testComplexFile() throws IOException {
+        Reader r = new InputStreamReader(getClass().getResourceAsStream("/sample.js"));
+        ITree tree = new RhinoTreeGenerator().generateFromReader(r).getRoot();
+        assertEquals(402, tree.getSize());
     }
 
 }
