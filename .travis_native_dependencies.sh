a9dbdd96ce6409f6dd9cf024d02f4401d0dab992
Jean-Rémy Falleri <jr.falleri@gmail.com>
2018-01-27 19:51:41 +0100

    fix path problems and verbose output of travis script

--- .travis_native_dependencies.sh
+++ .travis_native_dependencies.sh
@@ -4,14 +4,14 @@
 sudo apt-get update
 sudo apt-get install gcc g++ libxml2-dev libxslt1-dev libarchive-dev antlr libantlr-dev libcurl4-openssl-dev libssl-dev ocaml
 wget https://netix.dl.sourceforge.net/project/boost/boost/1.55.0/boost_1_55_0.tar.gz
-tar -xvzf boost_1_55_0.tar.gz
+tar -xzf boost_1_55_0.tar.gz
 cd boost_1_55_0
 ./bootstrap.sh --without-libraries=atomic,chrono,context,coroutine,exception,graph,graph_parallel,iostreams,locale,log,math,mpi,python,random,serialization,signals,test,timer,wave
 sudo ./b2 link=static cxxflags="-fPIC -static -Wl,--whole-archive" threading=multi install
 cd ..
 # then srcml itself
 wget http://131.123.42.38/lmcrs/beta/srcML-src.tar.gz
-tar -xvzf srcML-src.tar.gz
+tar -xzf srcML-src.tar.gz
 cd srcML-src
 cmake .
 make
