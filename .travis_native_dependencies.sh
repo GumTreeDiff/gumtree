6f81430178c3634c7a93738f9d3b00d77f0a6c91
Jean-Rémy Falleri <jr.falleri@gmail.com>
2018-01-27 19:22:26 +0100

    install srcml and gumtree from source on travis

---
+++ .travis_native_dependencies.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# 1) installing srcML
+# start by boost dependency
+sudo apt-get update
+sudo apt-get install gcc g++ libxml2-dev libxslt1-dev libarchive-dev antlr libantlr-dev libcurl4-openssl-dev libssl-dev ocaml
+wget https://netix.dl.sourceforge.net/project/boost/boost/1.55.0/boost_1_55_0.tar.gz
+tar -xvzf boost_1_55_0.tar.gz
+cd boost_1_55_0
+./bootstrap.sh --without-libraries=atomic,chrono,context,coroutine,exception,graph,graph_parallel,iostreams,locale,log,math,mpi,python,random,serialization,signals,test,timer,wave
+sudo ./b2 link=static cxxflags="-fPIC -static -Wl,--whole-archive" threading=multi install
+cd ..
+# then srcml itself
+wget http://131.123.42.38/lmcrs/beta/srcML-src.tar.gz
+tar -xvzf srcML-src.tar.gz
+cd srcML-src
+cmake .
+make
+cd ..
+# 2) installing cgum
+export PATH="$PWD/srcML-src/bin:$PATH"
+sudo apt-get install  ocaml ocaml-native-compilers camlp4
+git clone https://github.com/GumTreeDiff/cgum.git --depth 1
+cd cgum
+make
+export PATH="$PWD/cgum:$PATH"
