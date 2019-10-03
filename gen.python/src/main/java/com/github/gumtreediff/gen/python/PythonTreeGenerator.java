78d7b18228ff70807f7568a7e56e9282b4c62a2a
Jean-Rémy Falleri <jr.falleri@gmail.com>
2019-10-03 16:16:29 +0200

    update python parser generator now that python parser delivers the raw format

--- gen.python/src/main/java/com/github/gumtreediff/gen/python/PythonTreeGenerator.java
+++ gen.python/src/main/java/com/github/gumtreediff/gen/python/PythonTreeGenerator.java
@@ -23,6 +23,7 @@ import com.github.gumtreediff.gen.ExternalProcessTreeGenerator;
 import com.github.gumtreediff.gen.Register;
 import com.github.gumtreediff.gen.Registry;
 import com.github.gumtreediff.io.LineReader;
+import com.github.gumtreediff.io.TreeIoUtils;
 import com.github.gumtreediff.tree.ITree;
 import com.github.gumtreediff.tree.Type;
 import com.github.gumtreediff.tree.TreeContext;
@@ -41,74 +42,10 @@ public class PythonTreeGenerator extends ExternalProcessTreeGenerator {
 
     private static final String PYTHONPARSER_CMD = System.getProperty("gt.pp.path", "pythonparser");
 
-    private static final QName VALUE = new QName("value");
-
-    private static final QName LINENO = new QName("lineno");
-
-    private static final QName COL = new QName("col");
-
-    private static final QName END_LINENO = new QName("end_line_no");
-
-    private static final QName END_COL = new QName("end_col");
-
-    private LineReader lr;
-
-    private TreeContext context;
-
     @Override
     public TreeContext generate(Reader r) throws IOException {
-        lr = new LineReader(r);
-        String output = readStandardOutput(lr);
-        return getTreeContext(output);
-    }
-
-    public TreeContext getTreeContext(String xml) {
-        XMLInputFactory fact = XMLInputFactory.newInstance();
-        context = new TreeContext();
-        try {
-            ArrayDeque<ITree> trees = new ArrayDeque<>();
-            XMLEventReader r = fact.createXMLEventReader(new StringReader(xml));
-            while (r.hasNext()) {
-                XMLEvent ev = r.nextEvent();
-                if (ev.isStartElement()) {
-                    StartElement s = ev.asStartElement();
-                    Type type = type(s.getName().getLocalPart());
-                    String label = "";
-                    if (s.getAttributeByName(VALUE) != null)
-                        label = s.getAttributeByName(VALUE).getValue();
-                    ITree t = context.createTree(type, label);
-                    if (trees.isEmpty()) {
-                        context.setRoot(t);
-                    } else {
-                        t.setParentAndUpdateChildren(trees.peekFirst());
-                    }
-                    setPos(t, s);
-                    trees.addFirst(t);
-                } else if (ev.isEndElement())
-                    trees.removeFirst();
-            }
-            return context;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    private void setPos(ITree t, StartElement e) {
-        if (e.getAttributeByName(LINENO) == null) { //FIXME some nodes have start position
-            System.out.println(t.getLabel());
-            return;
-        }
-        int line = Integer.parseInt(e.getAttributeByName(LINENO).getValue());
-        int column = Integer.parseInt(e.getAttributeByName(COL).getValue());
-        t.setPos(lr.positionFor(line, column) + 2);
-        if (e.getAttributeByName(END_LINENO) == null) { //FIXME some nodes have no end position
-            System.out.println(t.getLabel());
-            return;
-        }
-        int endLine = Integer.parseInt(e.getAttributeByName(END_LINENO).getValue());
-        int endColumn = Integer.parseInt(e.getAttributeByName(END_COL).getValue());
-        t.setLength(lr.positionFor(endLine, endColumn) - lr.positionFor(line, column));
+        String output = readStandardOutput(r);
+        return TreeIoUtils.fromXml().generateFrom().string(output);
     }
 
     public String[] getCommandLine(String file) {
