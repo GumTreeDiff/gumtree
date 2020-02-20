9d73ad559530cab9c195c1ded166fd31cd13d475
Jean-Rémy Falleri <jr.falleri@gmail.com>
2020-02-20 16:49:14 +0100

    improve container infrastructure for travis and docker

--- .travis_native_dependencies.sh
+++ .travis_native_dependencies.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env sh
-# 1) installing srcML
+
+# 0) Installing dependencies
 sudo apt-get update
-sudo apt-get install wget gdebi-core
+sudo apt-get install gdebi-core ocaml ocaml-native-compilers camlp4
+
+# 1) installing srcML
 wget http://131.123.42.38/lmcrs/v1.0.0/srcml_1.0.0-1_ubuntu18.04.deb
 sudo gdebi srcml_1.0.0-1_ubuntu18.04.deb -n
+
 # 2) installing cgum
-sudo apt-get install ocaml ocaml-native-compilers camlp4
 git clone --branch v1.0.0 https://github.com/GumTreeDiff/cgum.git --depth 1
 cd cgum
 make
 cd ..
+
 # 3) installing pythonparser
 git clone https://github.com/GumTreeDiff/pythonparser.git --depth 1
