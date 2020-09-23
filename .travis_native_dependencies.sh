268c22751d51636655e77c927765c3a47aa1ec9c
Jean-Rémy Falleri <jr.falleri@gmail.com>
2020-09-23 10:24:19 +0200

    ci: update ci to use focal and the new python parser

--- .travis_native_dependencies.sh
+++ .travis_native_dependencies.sh
@@ -5,8 +5,8 @@ sudo apt-get update
 sudo apt-get install gdebi-core ocaml ocaml-native-compilers camlp4
 
 # 1) installing srcML
-wget http://131.123.42.38/lmcrs/v1.0.0/srcml_1.0.0-1_ubuntu18.04.deb
-sudo gdebi srcml_1.0.0-1_ubuntu18.04.deb -n
+wget http://131.123.42.38/lmcrs/v1.0.0/srcml_1.0.0-1_ubuntu20.04.deb
+sudo gdebi srcml_1.0.0-1_ubuntu20.04.deb -n
 
 # 2) installing cgum
 git clone --branch v1.0.0 https://github.com/GumTreeDiff/cgum.git --depth 1
