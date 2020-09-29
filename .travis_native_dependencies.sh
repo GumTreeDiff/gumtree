e3380b6d3b62bffaaf0e8312bcf57aac3c67255c
Jean-Rémy Falleri <jr.falleri@gmail.com>
2020-09-29 08:21:05 +0200

    ci: update travis build script.

--- .travis_native_dependencies.sh
+++ .travis_native_dependencies.sh
@@ -2,7 +2,7 @@
 
 # 0) Installing dependencies
 sudo apt-get update
-sudo apt-get install gdebi-core ocaml ocaml-native-compilers camlp4
+sudo apt-get install gdebi-core ocaml libnum-ocaml-dev
 
 # 1) installing srcML
 wget http://131.123.42.38/lmcrs/v1.0.0/srcml_1.0.0-1_ubuntu20.04.deb
@@ -15,4 +15,5 @@ make
 cd ..
 
 # 3) installing pythonparser
+pip3 install parso
 git clone https://github.com/GumTreeDiff/pythonparser.git --depth 1
