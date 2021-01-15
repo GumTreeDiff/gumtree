9dcbefd111c0a2dac61657f02df552d830584d3a
Jean-Rémy Falleri <jr.falleri@gmail.com>
2021-01-15 11:49:18 +0100

    fix: runs only size regression in CI.

--- benchmark/src/main/python/testRegressions.py
+++ benchmark/src/main/python/testRegressions.py
@@ -8,37 +8,27 @@ from scipy.stats import mannwhitneyu
 def main():
   ref_file = sys.argv[1]
   file = sys.argv[2]
-  testRegression(ref_file, file)
+  regression_type = sys.argv[3]
+  testRegressions(ref_file, file, regression_type)
 
-def testRegression(ref_file, file):
+def testRegressions(ref_file, file, regression_type):
   ref_data = pd.read_csv(ref_file, decimal=",", sep=";")
-  data = pd.read_csv(ref_file, decimal=",", sep=";")
+  data = pd.read_csv(file, decimal=",", sep=";")
   ref_algorithms = set(pd.unique(ref_data['algorithm']).tolist())
   algorithms = set(pd.unique(data['algorithm']).tolist())
   common_algorithms = ref_algorithms.intersection(algorithms)
-  # Test size regressions
-  print("Testing regressions for algorithms " + repr(common_algorithms))
+  print("Selected algorithms " + repr(common_algorithms))
   for algorithm in common_algorithms:
-    print("Checking script size regression for algorithm: " + algorithm)
-    ref_algorithm_data = ref_data[ref_data['algorithm'] == algorithm]['size']
-    algorithm_data = data[data['algorithm'] == algorithm]['size']
+    print("Checking " + regression_type + " regressions for algorithm: " + algorithm)
+    ref_algorithm_data = ref_data[ref_data['algorithm'] == algorithm][regression_type]
+    algorithm_data = data[data['algorithm'] == algorithm][regression_type]
     stat, p = mannwhitneyu(ref_algorithm_data, algorithm_data)
     if p < 0.05:
-      print("Detected script size regression for algorithm: " + algorithm)
+      print("Detected " + regression_type + " regressions for algorithm: " + algorithm)
+      print(p)
       sys.exit(1)
     else:
-      print("No script size regression for algorithm: " + algorithm)
-  # Test running time regression
-  for algorithm in common_algorithms:
-    print("Checking script size regression for algorithm: " + algorithm)
-    ref_algorithm_data = ref_data[ref_data['algorithm'] == algorithm]['runtime']
-    algorithm_data = data[data['algorithm'] == algorithm]['runtime']
-    stat, p = mannwhitneyu(ref_algorithm_data, algorithm_data)
-    if p < 0.05:
-      print("Detected runtime regression for algorithm: " + algorithm)
-      sys.exit(1)
-    else:
-      print("No runtime regression for algorithm: " + algorithm)
+      print("No " + regression_type + " regressions for algorithm: " + algorithm)
 
 if __name__ == "__main__":
     main()
