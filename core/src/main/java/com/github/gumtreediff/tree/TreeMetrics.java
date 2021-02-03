516f7d27328b109c7efe01260603cc4eb8c3908c
Jean-Rémy Falleri <jr.falleri@gmail.com>
2021-02-03 09:13:15 +0100

    doc-feat: documented treemetrics.

--- core/src/main/java/com/github/gumtreediff/tree/TreeMetrics.java
+++ core/src/main/java/com/github/gumtreediff/tree/TreeMetrics.java
@@ -20,17 +20,41 @@
 
 package com.github.gumtreediff.tree;
 
+/**
+ * Class containing several metrics information regarding a node of an AST.
+ * The metrics are immutables but lazily computed.
+ *
+ * @see Tree#getMetrics()
+ */
 public class TreeMetrics {
+    /**
+     * The number of nodes in the subtree rooted at the node.
+     */
     public final int size;
 
+    /**
+     * The size of the longer branch in the subtree rooted at the node.
+     */
     public final int height;
 
+    /**
+     * The hashcode of the subtree rooted at the node.
+     */
     public final int hash;
 
+    /**
+     * The hashcode of the subtree rooted at the node, excluding labels.
+     */
     public final int structureHash;
 
+    /**
+     * The number of ancestors of a node.
+     */
     public final int depth;
 
+    /**
+     * An absolute position for the node. Usually computed via the postfix order.
+     */
     public final int position;
 
     public TreeMetrics(int size, int height, int hash, int structureHash, int depth, int position) {
