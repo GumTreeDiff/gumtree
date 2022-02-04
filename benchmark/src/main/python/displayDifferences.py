7c478e06d7f08df60e57a59f7671ae5df5a8a05e
Jean-Rémy Falleri <jr.falleri@gmail.com>
2022-02-04 22:53:10 +0100

    fix: plot script for multiple time columns

--- benchmark/src/main/python/displayDifferences.py
+++ benchmark/src/main/python/displayDifferences.py
@@ -2,6 +2,7 @@
 
 import sys
 import pandas as pd
+import statistics
 from scipy.stats import mannwhitneyu
 
 def main():
@@ -13,7 +14,9 @@ def main():
 def displayDifferences(ref_file, file, difference_type):
   print(difference_type)
   ref_data = pd.read_csv(ref_file, decimal=",", sep=";")
+  ref_data['runtime'] = ref_data.apply (lambda row: statistics.median([row['t1'], row['t2'], row['t3'], row['t4'], row['t5']]), axis = 1)
   data = pd.read_csv(file, decimal=",", sep=";")
+  data['runtime'] = data.apply (lambda row: statistics.median([row['t1'], row['t2'], row['t3'], row['t4'], row['t5']]), axis = 1)
   ref_algorithms = set(pd.unique(ref_data['algorithm']).tolist())
   algorithms = set(pd.unique(data['algorithm']).tolist())
   common_algorithms = ref_algorithms.intersection(algorithms)
