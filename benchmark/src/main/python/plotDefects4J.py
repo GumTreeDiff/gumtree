108e9cbd75a7bc026646701c3f664a54a279a1b9
Jean-Rémy Falleri <jr.falleri@gmail.com>
2022-02-04 22:15:46 +0100

    fix: plot script for multiple time columns

--- benchmark/src/main/python/plotDefects4J.py
+++ benchmark/src/main/python/plotDefects4J.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+import statistics
 import pandas as pd
 from plotnine import *
 
@@ -10,9 +11,10 @@ def main():
 
 def plotCsv(file):
   data = pd.read_csv(file, decimal=",", sep=";")
-  plot = ggplot(data, aes(x='factor(algorithm)', y='size')) + geom_violin()
+  plot = ggplot(data, aes(x='factor(algorithm)', y='size')) + geom_jitter()
   plot.save(file + "_size.pdf")
-  plot = ggplot(data, aes(x='factor(algorithm)', y='runtime')) + geom_violin()
+  data['runtime'] = data.apply (lambda row: statistics.median([row['t1'], row['t2'], row['t3'], row['t4'], row['t5']]), axis = 1)
+  plot = ggplot(data, aes(x='factor(algorithm)', y='runtime')) + geom_jitter()
   plot.save(file + "_runtime.pdf")
 
 if __name__ == "__main__":
