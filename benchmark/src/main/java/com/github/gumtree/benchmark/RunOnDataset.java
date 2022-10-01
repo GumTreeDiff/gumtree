e8a5937dda42e0d50e76ad659cecbf9dad68e9f2
Jean-Rémy Falleri <jr.falleri@gmail.com>
2022-10-01 14:17:52 +0200

    fix: keep only simpleid in the benchamrk to avoid 1h CI running times

--- benchmark/src/main/java/com/github/gumtree/benchmark/RunOnDataset.java
+++ benchmark/src/main/java/com/github/gumtree/benchmark/RunOnDataset.java
@@ -74,7 +74,7 @@ public class RunOnDataset {
 
     private static final MatcherConfig[] configurations = new MatcherConfig[] {
             new MatcherConfig("SimpleId", CompositeMatchers.SimpleIdGumtree::new),
-            new MatcherConfig("Classic", CompositeMatchers.ClassicGumtree::new),
+            //new MatcherConfig("Classic", CompositeMatchers.ClassicGumtree::new),
             //new MatcherConfig("HybridId", () -> new CompositeMatchers.HybridIdGumtree()),
     };
 
