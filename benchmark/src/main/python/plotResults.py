294aa6b589cfb9abd262541a5b7c79e8ab7ff47c
Jean-Rémy Falleri <jr.falleri@gmail.com>
2023-06-30 15:17:41 +0200

    fix: use log10 transformation for running time

--- benchmark/src/main/python/plotResults.py
+++ benchmark/src/main/python/plotResults.py
@@ -19,7 +19,7 @@ def plotCsv(file):
   plot = ggplot(data, aes(x='factor(algorithm)', y='s', fill='factor(algorithm)')) + geom_violin(draw_quantiles=[0.25, 0.5, 0.75], show_legend=False) + scale_y_continuous(trans="log1p", breaks = [0, 1, 5, 10, 20, 50, 100, 1000]) + ylab("Number of actions") + xlab("Algorithm") + scale_fill_manual(values=cb_palette()) + theme_light()
   plot.save(file + "_size.pdf")
   data['runtime'] = data.apply (lambda row: statistics.median([row['t'], row['t.1'], row['t.2'], row['t.3'], row['t.4']]), axis = 1)
-  plot = ggplot(data, aes(x='factor(algorithm)', y='runtime / 1_000_000_000', fill='factor(algorithm)')) + geom_violin(draw_quantiles=[0.25, 0.5, 0.75], show_legend=False) + scale_y_continuous(trans="log1p", breaks = [0, 0.5, 1, 10, 100, 1000]) + ylab("Runtime (s)") + xlab("Algorithm") + scale_fill_manual(values=cb_palette()) + theme_light()
+  plot = ggplot(data, aes(x='factor(algorithm)', y='runtime / 1_000_000_000', fill='factor(algorithm)')) + geom_violin(draw_quantiles=[0.25, 0.5, 0.75], show_legend=False) + scale_y_continuous(trans="log10", breaks = [0, 0.5, 1, 10, 100, 1000]) + ylab("Runtime (s)") + xlab("Algorithm") + scale_fill_manual(values=cb_palette()) + theme_light()
   plot.save(file + "_runtime.pdf")
 
 def cb_palette():
