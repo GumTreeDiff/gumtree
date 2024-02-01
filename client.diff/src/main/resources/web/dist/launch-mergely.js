dd8cd10420e7e99ed3c756129976adf1db7f5fe5
Jean-Rémy Falleri <jr.falleri@gmail.com>
2024-02-01 21:11:18 +0100

    fix: more readable asynchronous code

--- client.diff/src/main/resources/web/dist/launch-mergely.js
+++ client.diff/src/main/resources/web/dist/launch-mergely.js
@@ -1,13 +1,13 @@
 const doc = new Mergely('#compare', { cmsettings : { readOnly: true, lineNumbers: true, lineWrapping: true }});
 doc.once('updated', () => {
-    Promise.all([fetch(lhs_url), fetch(rhs_url)]).then((values) => {
-        Promise.all([values[0].text(), values[1].text()]).then((values) => {
-            doc.lhs(values[0]);
-            doc.rhs(values[1]);
-
-            doc.once('updated', () => {
-                doc.scrollToDiff('next');
-            });
+    Promise.all([
+        fetch(lhs_url).then(result => result.text()),
+        fetch(rhs_url).then(result => result.text())
+    ]).then(([lhs, rhs]) => {
+        doc.lhs(lhs);
+        doc.rhs(rhs);
+        doc.once('updated', () => {
+            doc.scrollToDiff('next');
         });
     });
 });
